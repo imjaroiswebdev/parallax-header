7398f0f20f3a06b7185a3009495268db34248b56
José Reyes <imjaroiswebdev@gmail.com>
2017-02-20 01:42:14 -0400

    Limpieza estetcia de app.js.

--- app/js/app.js
+++ app/js/app.js
@@ -1,23 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     
-    var headerH = document.querySelector('.parallax-header').offsetHeight;
-    var logo = document.querySelector('.parallax-header__logo').style;
-    var choco = document.querySelector('.parallax-header__choco').style;
-    var toppers = document.querySelector('.parallax-header__toppers').style;
+    var headerH = document.querySelector('.parallax-header').offsetHeight,
+        logo = document.querySelector('.parallax-header__logo').style,
+        choco = document.querySelector('.parallax-header__choco').style,
+        toppers = document.querySelector('.parallax-header__toppers').style;
     
-    function parallaxFx () {
+    function parallaxFx() {
         var yOffset = window.pageYOffset;
         
         if (yOffset <= headerH) {
-            var transparency = 1 - (yOffset/headerH)*1.2; // Desaparación de los elementos acelerada en un 20%
-            logo.transform = 'translateY(' + yOffset/1.5 + 'px)';
+            var transparency = 1 - (yOffset / headerH) * 1.2; // Desaparación de los elementos acelerada en un 20%
+            logo.transform = 'translateY(' + yOffset / 1.5 + 'px)';
             
-            choco.transform = 'translateY(' + yOffset/30 + 'px)';
+            choco.transform = 'translateY(' + yOffset / 30 + 'px)';
             choco.opacity = transparency;
-            toppers.transform = 'translateY(' + yOffset/30 + 'px)';
+            toppers.transform = 'translateY(' + yOffset / 30 + 'px)';
             toppers.opacity = transparency;
         }
-    };
+    }
     
     window.addEventListener('scroll', parallaxFx);
-})
+});
